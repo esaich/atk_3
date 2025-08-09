191c6deb612cd6e8cdd909d87cbe39333c043f2a
esaich <saidhamzah12@gmail.com>
2025-08-09 18:04:59 +0700

    ...

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,8 +9,8 @@
     <meta content="" name="keywords">
 
     {{-- Favicons --}}
-    <link href="{{ asset('assets/img/Kerawang.png') }}" rel="icon">
-    <link href="{{ asset('assets/img/Kerawang.png') }}" rel="apple-touch-icon">
+    {{-- <link href="{{ asset('assets/img/Kerawang.png') }}" rel="icon">
+    <link href="{{ asset('assets/img/Kerawang.png') }}" rel="apple-touch-icon"> --}}
 
     {{-- Google Fonts --}}
     <link href="https://fonts.gstatic.com" rel="preconnect">
